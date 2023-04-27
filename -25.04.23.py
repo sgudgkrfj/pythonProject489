487f3b37af7f8bedd931cda807e1283d0a3d7c90
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-04-27 18:48:16 +0300

    kwargs

--- -25.04.23.py
+++ -25.04.23.py
@@ -4,7 +4,15 @@ print('bsvbwe',2123,9.0)
 def black_hole(*args):
     print(type(args))
     print(args)
-    for arguments in args:
+    for argument in args:
         print(argument)
 
 black_hole(234,"Earth","rusnia","time")
+
+
+def black_hole_named(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+    for argument in kwargs:
+        print(argument)
+black_hole_named(name="Gleb", planet="Earth",number=5)
