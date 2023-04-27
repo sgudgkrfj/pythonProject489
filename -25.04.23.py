077712db75733d3110f8de1c8da5e9c1d4e87587
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-04-27 18:39:58 +0300

    args

--- -25.04.23.py
+++ -25.04.23.py
@@ -1,30 +1,10 @@
-"""Задано список з цілих чисел. Напишіть функцію, яка
-повертає суму всіх елементів цього списку. Напишіть код,
-що демонструє роботу функції та показує суму елементів у
-деяких списках."""
+#теорія
+print('bsvbwe',2123,9.0)
 
-lst=[1,2,3,4,5]
-def listochek(lst):
-    return(sum(lst))
-print(listochek(lst))
+def black_hole(*args):
+    print(type(args))
+    print(args)
+    for arguments in args:
+        print(argument)
 
-"""Напишіть функцію, яка приймає рядок та повертає
-True, якщо рядок є паліндромом, та False в іншому випадку"""
-
-def string1():
-    str1=input("Введіть рядок: ")
-    if str1[::-1]==str1[::]:
-        return True
-    elif str1[::-1]!=str1[::]:
-        return False
-print(string1())
-
-
-""" Створіть функцію, яка приймає список рядків та повертає
-список рядків, відсортованих за зростанням довжини."""
-
-
-s=["63h14e44","jh5jtdfyt","kuildtj"]
-def list(s):
-    return sorted(s, key=len)
-print(list(s))
+black_hole(234,"Earth","rusnia","time")
